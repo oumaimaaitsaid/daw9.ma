140ffb03cc97fe37554eb63442e92c270f153bd1
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2026-01-28 10:00:00 +0100

    feat: implement MoodboardService for image uploads

---
+++ frontEnd/src/app/core/services/moodboard.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { MoodboardImage } from '../models/moodboard.model';
+
+@Injectable({ providedIn: 'root' })
+export class MoodboardService {
+  private http = inject(HttpClient);
+  private baseUrl = `${environment.apiUrl}/client/moodboard`;
+
+  getMoodboard(): Observable<{ content: MoodboardImage[] }> {
+    return this.http.get<{ content: MoodboardImage[] }>(this.baseUrl);
+  }
+
+  uploadBulk(formData: FormData): Observable<MoodboardImage[]> {
+    return this.http.post<MoodboardImage[]>(`${this.baseUrl}/upload-bulk`, formData);
+  }
+
+  deleteImage(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
+
+  getMatches(): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/match`);
+  }
+}
