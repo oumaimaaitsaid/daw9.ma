ad68f2cd93df8ceeafc04d6c7553053ef57b6e50
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2026-02-21 09:45:00 +0000

    feat: create main application entry point

---
+++ backEnd/src/main/java/com/daw9/Daw9Application.java
@@ -0,0 +1,12 @@
+package com.daw9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Daw9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Daw9Application.class, args);
+    }
+}
