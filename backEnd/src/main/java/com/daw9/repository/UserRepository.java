8ae30e734c4a4e7931740c729dee92cf56b7b98e
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2026-02-21 15:30:00 +0000

    feat: add UserRepository interface

---
+++ backEnd/src/main/java/com/daw9/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.daw9.repository;
+
+import com.daw9.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
