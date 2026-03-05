6e0cb640ce495799bfb3a5aef18c57748d00ad0a
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2026-03-05 11:30:00 +0000

    feat: configure cache management for catalogue queries

---
+++ backEnd/src/main/java/com/daw9/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package com.daw9.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+}
