63ba714a09d74600b241e3d45107d9d1110d966c
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2026-03-06 09:00:00 +0000

    feat: define NotificationType enum

---
+++ backEnd/src/main/java/com/daw9/model/enums/NotificationType.java
@@ -0,0 +1,16 @@
+package com.daw9.model.enums;
+
+public enum NotificationType {
+    // Client
+    NOUVEAU_PRESTATAIRE_COMPATIBLE,
+    RESERVATION_CONFIRMEE,
+    RESERVATION_ANNULEE,
+
+    // Prestataire
+    NOUVEAU_CLIENT_COMPATIBLE,
+    PAIEMENT_RECU,
+
+    // Admin
+    PRESTATAIRE_A_VALIDER,
+    LITIGE_SIGNALE
+}
